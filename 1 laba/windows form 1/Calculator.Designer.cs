0a8da4dd6272f904d10be85680e3d9dba187328a
anastasdrozhzha <71096457+anastasdrozhzha@users.noreply.github.com>
2021-02-20 18:34:25 +0300

    Add files via upload

---
+++ 1 laba/windows form 1/Calculator.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace _1.Windows_form
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 40);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Input your integer numbers \r\nto do different manipulations";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(39, 129);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 34);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "And";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(137, 129);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(80, 34);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "Not";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(236, 129);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(80, 34);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "Or";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Location = new System.Drawing.Point(137, 178);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(80, 34);
+            this.button8.TabIndex = 21;
+            this.button8.Text = "XOR";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button9.Location = new System.Drawing.Point(268, 69);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(80, 34);
+            this.button9.TabIndex = 22;
+            this.button9.Text = "Delete";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(53, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(263, 20);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Choose a notation for watching results";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 322);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(277, 20);
+            this.textBox1.TabIndex = 33;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(39, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(217, 20);
+            this.textBox2.TabIndex = 34;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_keyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(39, 57);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(217, 20);
+            this.textBox3.TabIndex = 35;
+            this.textBox3.Tag = "write";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_keyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(97, 291);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 20);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "Result in a standart veiw";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(90, 353);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(176, 20);
+            this.label4.TabIndex = 37;
+            this.label4.Text = "Result in a other notation";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(39, 387);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(277, 20);
+            this.textBox4.TabIndex = 38;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(39, 257);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(63, 17);
+            this.radioButton1.TabIndex = 43;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Decimal";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(108, 257);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(86, 17);
+            this.radioButton2.TabIndex = 44;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Hexadecimal";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged_1);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(206, 257);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(50, 17);
+            this.radioButton3.TabIndex = 45;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Octal";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(262, 257);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(54, 17);
+            this.radioButton4.TabIndex = 46;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Binary";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(360, 435);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Name = "Calculator";
+            this.Text = "Binary calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
+
---
+++ 1 laba/windows form 1/Calculator.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1.Windows_form
+{
+    
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+            radioButton1.CheckedChanged += new EventHandler(radioButton1_CheckedChanged);
+            radioButton2.CheckedChanged += new EventHandler(radioButton2_CheckedChanged);
+            radioButton3.CheckedChanged += new EventHandler(radioButton3_CheckedChanged);
+            radioButton4.CheckedChanged += new EventHandler(radioButton4_CheckedChanged);
+        }
+
+        private int funcAnd(int a, int b)
+        {
+            return a & b;
+        }
+
+        private int funcOr(int a, int b)
+        {
+            return a | b;
+        }
+
+        private int funcXor(int a, int b)
+        {
+            return a ^ b;
+        }
+
+        private int funcNot(int a)
+        {
+            return ~a ;
+        }
+       
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString(funcAnd(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text)));
+           
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString(funcOr(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text)));
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+           textBox1.Text = Convert.ToString(funcNot(Convert.ToInt32(textBox3.Text)));
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString(funcXor(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text)));
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int i = Convert.ToInt32(textBox1.Text);
+                textBox4.Text = Convert.ToString(i, 10);
+            }
+            catch (FormatException) { };
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            try 
+            { 
+                int i = Convert.ToInt32(textBox1.Text);
+                textBox4.Text = Convert.ToString(i, 16);
+            }
+            catch (FormatException) { };
+}
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int i = Convert.ToInt32(textBox1.Text);
+                textBox4.Text = Convert.ToString(i, 8);
+            }
+            catch (FormatException) { };
+        }
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int i = Convert.ToInt32(textBox1.Text);
+                textBox4.Text = Convert.ToString(i, 2);
+            }
+            catch (FormatException) { };
+        }
+
+        private void textBox3_keyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                char number = e.KeyChar;
+                if (!Char.IsDigit(number))
+                {
+                    e.Handled = true;
+                    textBox3.BackColor = Color.Red;
+                }
+                else textBox3.BackColor = SystemColors.Window;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Error, you input wrong numbers!Try again");
+            }
+        }
+
+        private void textBox2_keyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                char number = e.KeyChar;
+                if (!Char.IsDigit(number))
+                {
+                    e.Handled = true;
+                    textBox2.BackColor = Color.Red;
+                }
+                else textBox2.BackColor = SystemColors.Window;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Error, you input wrong numbers!Try again");
+            }
+        }
+        private void radioButton2_CheckedChanged_1(object sender, EventArgs e)
+        {
+           
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
